3e0e4c7fa2d25d263d17eeb9c9fccb4766052526
Carlos Paparoni <carlos.paparoni.fsl@crowdbotics.com>
2023-04-13 19:38:39 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "recipehelloworld-39916",
-  url: "https://recipehelloworld-39916.botics.co",
-  api: "https://recipehelloworld-39916.botics.co/api/v1"
-}
+  "name": "recipehelloworld-39916",
+  "url": "https://recipehelloworld-39916.botics.co",
+  "api": "https://recipehelloworld-39916.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
