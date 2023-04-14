2e810c2b485d0c0686452092ffe0191cd0c5967b
Crowdbotics <engineering@crowdbotics.com>
2023-04-14 12:26:25 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,11 +1,18 @@
 export const globalOptions = {
-  "name": "recipehelloworld-39916",
-  "url": "https://recipehelloworld-39916.botics.co",
-  "api": "https://recipehelloworld-39916.botics.co/api/v1",
-  "initialRoute": "Untitled1"
-};
+  name: "recipehelloworld-39916",
+  url: "https://recipehelloworld-39916.botics.co",
+  api: "https://recipehelloworld-39916.botics.co/api/v1",
+  initialRoute: "Untitled1"
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
+  },
+  undefined: {
+    duration: 5,
+    onDurationEnd: onDurationEnd,
+    hide: hide,
+    show: show
   }
-};
+}
