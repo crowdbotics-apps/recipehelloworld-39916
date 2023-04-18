9f4fbec2f4dc2b0fb271eb1fe3f15fee97d00de4
Crowdbotics <engineering@crowdbotics.com>
2023-04-18 01:10:34 +0000

    Update store.

--- store/test/api.js
+++ store/test/api.js
@@ -1,9 +1,6 @@
-import axios from "axios";
+import axios from "axios"
 const test = axios.create({
   baseURL: "https://www.google.com",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-export const apiService = {};
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
